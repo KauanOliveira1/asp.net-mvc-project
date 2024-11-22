236356ac689b5597001a7b6f82623fd5252210fb
KauanOliveira1 <oliveirakauan7@gmail.com>
2024-11-21 22:24:34 -0300

    initial commit

---
+++ controle-contatos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace controle_contatos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
